3020162927d2c01f786c31d8d2c88303b9edd6ef
Vicen <vicenteadm04@gmail.com>
2025-05-25 18:43:40 -0300

    flake8

--- test_suma.py
+++ test_suma.py
@@ -2,10 +2,10 @@ from suma import suma
 
 
 def test_suma_positivo():
-    assert suma(4,5.5) == 9.5
+    assert suma(4, 5.5) == 9.5
 
 def test_suma_negativo():
-    assert suma(-3,-5) == -8
+    assert suma(-3, -5) == -8
 
 def test_suma_mixto():
-    assert suma (3,-5) == -2
+    assert suma (3, -5) == -2
