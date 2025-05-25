236f83d1e94f449c0e16b6e3fc2e23ef77994b7f
Vicen <vicenteadm04@gmail.com>
2025-05-25 18:56:11 -0300

    sin logger

--- suma.py
+++ suma.py
@@ -1,16 +1,8 @@
-#from logger_config  import get_logger
-
-
-#logger = get_logger('suma')
 def suma(a,b):
+    
+    resultado = a + b
+    return resultado
 
-    #try:
-        resultado = a + b
-        #logger.info(f"suma. {a} + {b} = {resultado}")
-        return resultado
-    #except TypeError as e:
-     #   logger.error(f"Error al sumar: a={a},  b={b} -> {e}")
-      #  return None
 
 
 
